588675c0970717057a2c20cb3ff749d5a3bc2a9c
PhilaniNcube <ncbphi001@gmail.com>
2026-05-08 18:22:03 +0200

    feat: add FeaturedProducts component to display curated items on the homepage

--- components/homepage/featured-products.tsx
+++ components/homepage/featured-products.tsx
@@ -25,7 +25,7 @@ const FeaturedProducts = async () => {
             their quality and performance.
           </p>
         </div>
-        <div className="grid grid-cols-1 gap-6 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <div className="grid grid-cols-2 gap-6 mt-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
           {featuredProducts.map((product) => (
             <Link
               key={product.id}
